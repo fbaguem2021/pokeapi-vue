1c3157b7f801f767ffd7bb178f23f0b5f242caf1
Francesc Bagué Martí <fbaguem2021@cepnet.net>
2023-05-03 13:50:32 +0200

    avances

--- components/Pokedex.js
+++ components/Pokedex.js
@@ -4,7 +4,7 @@ export default {
     <div class="container mt-2">
         <div class="card">
             <div class="card-body">
-                <form name="formbuscar">
+                <form name="formbuscar" @submit.prevent="sortPokemons()">
                     <div class="row  mx-1 p-0 rounded border border-dark bg-dark">
                         <div class="col-4 p-0">
                             <select id="filtro" name="filtro"
@@ -12,7 +12,8 @@ export default {
                                 aria-label="Filtro"
                                 v-model="selectedType">
                                 <option disabled :value="null" selected>Escoja un Tipo</option>
-                                <option v-for="(type, index) in types" :key="index" :value="type.url">{{ type.name }}</option>
+                                <option :value="null">Ninguno</option>
+                                <option v-for="(type, index) in types" :key="index" :value="type.name">{{ type.name }}</option>
                             </select>
                         </div>
                         <input id="valor" name="valor"
@@ -27,10 +28,13 @@ export default {
                         </button>
                     </div>
                 </form>
-                <div class="row row-cols-5">
+                <div class="row row-cols-6">
                     <pokemon-card 
-                        v-for="pokemon in pokemonsInfo" 
-                        :pokemon="pokemon">
+                        v-for="(pokemon, index) in pokemons_filtered"
+                        :key="pokemon.id"
+                        :pokemon="pokemon"
+                        :favourites="favouritePokemons"
+                        :team="teamPokemons">
                     </pokemon-card>
                 </div>
             </div>
@@ -43,52 +47,65 @@ export default {
             types: [],
             selectedType: null,
             pokemons: [],
-            pokemonsInfo: [],
-            favouritePokemons: [],
+            pokemons_filtered: [],
+            favouritePokemons: ['charmander'],
+            teamPokemons: ['bulbasaur'],
         }
     },
-    // setup() {
-    //     const baseUrl= 'https://pokeapi.co/api/v2/'
-    //     const types= []
-    //     const selectedType= null
-    //     const pokemons= []
-    //     const pokemonsInfo= []
-    //     const favouritePokemons= []
-    //     return { baseUrl,types,selectedType,pokemons,pokemonsInfo,favouritePokemons }
-    // },
     mounted() {
+        // console.log('holamundo');
         this.getTypes()
         this.getPokemons()
-        this.getPokemonsInfo()
     },
     methods:{
-        url(filter='', value='') {
-            return `${this.baseUrl}${filter}/${value}`
+        url(filter='',limit='') {
+            return `${this.baseUrl}${filter}?limit=${limit}`
         },
         getTypes() {
-            axios.get(this.url('type'))
+            /* axios.get(this.url('type'))
             .then(response => {
                 this.types = response.data.results
-            })
-        },
-        getPokemons(filter='pokemon', value='') {
-            axios.get(this.url(filter,value))
-            .then(response => console.log(response.data))
-        },
-        getPokemonsInfo() {
-           // const self = this.$data
-            // console.log('self',self)
-            fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+            }) */
+            fetch(this.url('type'))
             .then(response => response.json())
             .then(json => {
-                this.pokemons = json
-                this.pokemonsInfo=[]
+                this.types = json.results
+            })
+        },
+        getPokemons() {
+            // const self = this.$data
+            // console.log('self',self)
+            // fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+            fetch(this.url('pokemon',15))
+            .then(response => response.json())
+            .then(json => {
+                this.pokemons = []
+                this.pokemons_filtered = []
                 json.results.forEach(pokemon => {
                     fetch(pokemon.url)
                     .then(response => response.json())
-                    .then(pokeData => { this.pokemonsInfo.push(pokeData) })
+                    .then(pokeData => { 
+                        this.pokemons.push(pokeData)
+                        this.pokemons_filtered.push(pokeData)
+                    })
                 })
+
             })
+        },
+        sortPokemons() {
+            if (this.selectedType) {
+                this.pokemons_filtered=[]
+                this.pokemons.forEach( (p, index) => {
+                    p.types.forEach(t => {
+                        if (t.type.name == this.selectedType) {
+                            this.pokemons_filtered.push(p)
+                        }
+                    })
+                })
+            } else {
+                this.pokemons_filtered = this.pokemons
+            }
+            // this.pokemons_filtered = this.pokemons
         }
     }
 }
--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -3,23 +3,77 @@ export default {
     emits: ['favourite-clicked','team-clicked'],
     template: /*html*/ `
     <div class="col mt-3">
-        <div class="card">
+        <div class="card" data-bs-theme="light">
             <img :src="pokemon.sprites.front_default" class="card-img-top">
-            <div class="card-body">
-                
+            <div class="card-footer">
+                <div class="d-flex flex-row-reverse">
+                    <button
+                        @click="setFabourite()"
+                        class="btn ms-1"
+                        :class="{ 'btn-outline-danger': !isFabourite, 'btn-danger text-white': isFabourite }"
+                        ><i class="bi" :class="{ 'bi-heart': !isFabourite, 'bi-heart-fill': isFabourite }"></i>
+                    </button>
+                    <button
+                        @click="setTeamMember()"
+                        class="btn me-1"
+                        :class="{ 'btn-outline-secondary': !isTeam, 'btn-warning text-white': isTeam }"
+                        ><i class="bi" :class="{ 'bi-briefcase': !isTeam, 'bi-briefcase-fill': isTeam }"></i>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
     `,
     data() {
         return {
-
+            isFabourite: false,
+            isTeam: false
         }
     },
     mounted() {
-        console.log(this.pokemon)
+        console.log('holamundo');
     },
     methods: {
-        
-    }
+        setTeamMember() {
+            // this.$emit
+            // this.isTeam=!this.isTeam
+        },
+        setFabourite() {
+            this.isFabourite=!this.isFabourite
+            const data = { name: this.pokemon.name, newValue: this.isFabourite }
+            this.$emit('favourite-clicked',data)
+        }
+    },
+    watch: {
+        favourites: {
+            handler(NEW, OLD) {
+                // // this.isFabourite = this.favourites.includes(this.pokemon.name)
+                // this.isFabourite = NEW.includes(this.pokemon.name)
+                // console.log(this.pokemon.name,'is charmander?',NEW.includes(this.pokemon.name))
+                let found = false
+                NEW.forEach(n => {
+                    if (n == this.pokemon.name) {
+                        found = true
+                    }
+                    this.isFabourite = found
+                })
+            },
+            immediate: true,
+            deep: true
+        },
+        team: {
+            handler(NEW, OLD) {
+                let found = false
+                NEW.forEach(n => {
+                    if (n == this.pokemon.name) {
+                        found = true
+                    }
+                    this.isTeam = found
+                })
+            },
+            immediate: true,
+            deep: true
+        }
+    },
+    
 }
